6a6c25f38c6507cf07ed3223f1ed81cf4c767b91
christian <christiaanmillaan1313@gmail.com>
2026-03-08 18:11:32 +0100

    relocate logo images and fonts folder; set background color variable and brand font variable; place logo and brand name in header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
 import type { Metadata } from "next"
 import "./globals.css"
+import localfont from "next/font/local"
 
 export const metadata: Metadata = {
   title: "Orbit",
   description: "Find the cinematic connection between any two stars."
 }
+
+const brandFont = localfont({
+  src: "../fonts/fixga/regular.woff2",
+  variable: "--font-brand"
+})
 
 export default function RootLayout({
   children
@@ -12,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang = "en">
-      <body>
+    <html className = {brandFont.variable} lang = "en">
+      <body className = "bg-background p-5">
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
+import Image from "next/image"
+
 export default function Home() {
   return (
-    <></>
+    <header className = "flex items-center justify-between">
+      <div className = "flex gap-3 items-center">
+        <Image
+          alt = "Logo"
+          height = {30}
+          priority
+          src = "/logo/logo.png"
+          width = {30}
+        />
+
+        <h1 
+          className = "font-brand tracking-wider text-3xl text-white uppercase font-bold"
+          style = {{
+            textShadow: "0 0 10px #00ffffcc, 0 0 20px #00ffff99, 0 0 30px #00ffff66, 0 0 40px #00ffff33"
+          }}
+        >
+          Orbit
+        </h1>
+      </div>
+    </header>
   )
 }
